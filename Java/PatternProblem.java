fd70802cf51c877e5dab45b92fdf80a336d8598e
Developer World <teamdeveloperworld@gmail.com>
2024-01-30 17:04:47 +0530

    added few more pattern

--- Java/PatternProblem.java
+++ Java/PatternProblem.java
@@ -13,10 +13,17 @@ public class PatternProblem {
         sc.close();
         int rowIndex;
         int colIndex;
-        //looping 
+        //printing square or rectangle
         for(rowIndex=1;rowIndex<=rowNumber;rowIndex++){
             for(colIndex=1;colIndex<=colNumber;colIndex++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+        //printing the same no in that column for n row
+        for(rowIndex=1;rowIndex<=rowNumber;rowIndex++){
+            for(colIndex=1;colIndex<=rowIndex;colIndex++){
+                System.out.print(rowIndex);
             }
             System.out.println();
         }
